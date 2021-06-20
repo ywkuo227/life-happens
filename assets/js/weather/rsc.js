591af61fca61e7916c0bf688a7688c2b95a21561
ywkuo227 <ywkuo@outlook.com>
2021-06-20 02:03:04 -0700

    June 20th code update #1: - Implement Weather UI and Codebase. - Tweaked Weather UI appearence. - Tweaked Hub.html appearence. - Add city of interest is WIP.

---
+++ assets/js/weather/rsc.js
@@ -0,0 +1,27 @@
+var appID = "09f53d37e1a1adeb77d0a6b88629e1fe";
+
+// REMOVE BEFORE FLIGHT: Inject fake test data into Local Storage.
+// function injectTestData() {
+//     var aryUserInfo = {
+//         userName: {
+//         firstName: "Jessica",
+//         lastName: "Guo",
+//         },
+//         location: {
+//             city: "Qingdao",
+//             latitude: "",
+//             longtitude: ""
+//         },
+//         savedWeather: [
+//             "Seattle",
+//             "Shanghai"
+//         ],
+//         savedToDo: [
+//         ]
+//     };
+    
+//     localStorage.clear();
+//     localStorage.setItem("UsrInfo", JSON.stringify(aryUserInfo));
+// }
+
+// injectTestData();
---
+++ assets/js/weather/weather.js
@@ -0,0 +1,155 @@
+var aryUserInfo = JSON.parse(localStorage.getItem("UsrInfo"));
+
+function displayLocalWeather() {
+    fetch(genWeatherQueryURL(aryUserInfo.location.city))
+        .then((response) => {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then((weatherData) => {
+
+            fetch(genOneCallQueryURL(weatherData.coord.lat, weatherData.coord.lon))
+                .then((response) => {
+                    if (!response.ok) {
+                        throw response.json();
+                    }
+                    return response.json();
+                })
+                .then((oneCallData) => {
+                    var uviColor;
+
+                    $("#dispDailyWeather").empty();
+                    $("#weekForecast").empty();
+
+                    if (oneCallData.current.uvi <= 2.99) {
+                        uviColor = "uvi-low";
+                    } else if (oneCallData.current.uvi >= 3 && oneCallData.current.uvi <= 5.99) {
+                        uviColor = "uvi-medium";
+                    } else if (oneCallData.current.uvi >= 6 && oneCallData.current.uvi <= 7.99) {
+                        uviColor = "uvi-high";
+                    } else if (oneCallData.current.uvi >= 8 && oneCallData.current.uvi <= 10.99) {
+                        uviColor = "uvi-very-high";
+                    } else if (oneCallData.current.uvi >= 11) {
+                        uviColor = "uvi-extreme";
+                    }
+
+                    $("#dispDailyWeather").append(`
+                        <div class="columns">
+                            <i class="column is-one-quarter weather-icon">
+                                <img src="${genIconURL(weatherData.weather[0].icon)}" height="100px" width="100px" alt="${weatherData.weather[0].description}">
+                            </i>
+                            <div class="column">
+                                <div class="columns">
+                                    <div class="column is-full">
+                                        ${weatherData.name}
+                                        <div class="columns">
+                                            <div class="column is-half">
+                                                ${weatherData.weather[0].main}  -  ${Math.round(weatherData.main.temp)}°F<br>
+                                                Wind:  ${Math.round(weatherData.wind.speed * 10) / 10} MPH
+                                            </div>
+                                            <div class="column">
+                                                UV Index:  <span class="p-1 uvi ${uviColor}">${Math.round(oneCallData.current.uvi * 10) / 10}</span><br>
+                                                Humidity:  ${weatherData.main.humidity}%
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+
+                    for (i = 1; i < 8; i++) {
+                        $("#weekForecast").append(`
+                            <div class="forecastContent">
+                                <div>
+                                    ${moment.unix(oneCallData.daily[i].dt).format("ddd")}
+                                </div>
+                                <i>
+                                <img src="${genIconURL(oneCallData.daily[i].weather[0].icon)}" alt="${oneCallData.daily[i].weather.description}">
+                                </i>
+                                <div>
+                                    ${Math.round(oneCallData.daily[i].temp.day)}°F
+                                </div>
+                            </div>
+                        `)
+                    }
+                })
+        })
+}
+
+function displayRegionalWeather() {
+    $("#interestedWeather").empty();
+    for (i = 0; i < aryUserInfo.savedWeather.length; i++) {
+        fetch(genWeatherQueryURL(aryUserInfo.savedWeather[i]))
+            .then((response) => {
+                if (!response.ok) {
+                    throw response.json();
+                }
+                return response.json();
+            })
+            .then((weatherData) => {
+
+                fetch(genOneCallQueryURL(weatherData.coord.lat, weatherData.coord.lon))
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw response.json();
+                        }
+                        return response.json();
+                    })
+                    .then((oneCallData) => {
+                        var uviColor;
+
+                        if (oneCallData.current.uvi <= 2.99) {
+                            uviColor = "uvi-low";
+                        } else if (oneCallData.current.uvi >= 3 && oneCallData.current.uvi <= 5.99) {
+                            uviColor = "uvi-medium";
+                        } else if (oneCallData.current.uvi >= 6 && oneCallData.current.uvi <= 7.99) {
+                            uviColor = "uvi-high";
+                        } else if (oneCallData.current.uvi >= 8 && oneCallData.current.uvi <= 10.99) {
+                            uviColor = "uvi-very-high";
+                        } else if (oneCallData.current.uvi >= 11) {
+                            uviColor = "uvi-extreme";
+                        }
+
+                        $("#interestedWeather").append(`
+                            <div class="columns mx-1">
+                                <i class="column is-one-quarter weather-icon">
+                                    <img src="${genIconURL(weatherData.weather[0].icon)}" height="100px" width="100px" alt="${weatherData.weather[0].description}">
+                                </i>
+                                <div class="column">
+                                    <div class="columns">
+                                        <div class="column is-full">
+                                            ${weatherData.name}
+                                            <div class="columns">
+                                                <div class="column is-half">
+                                                    ${weatherData.weather[0].main}  -  ${Math.round(weatherData.main.temp)}°F<br>
+                                                    Wind:  ${Math.round(weatherData.wind.speed * 10) / 10} MPH
+                                                </div>
+                                                <div class="column">
+                                                    UV Index:  <span class="p-1 uvi ${uviColor}">${Math.round(oneCallData.current.uvi * 10) / 10}</span><br>
+                                                    Humidity:  ${weatherData.main.humidity}%
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        `);
+                    })
+            })
+    }
+}
+
+// REMOVE BEFORE FLIGHT: pageInitialization
+function pgInitialize() {
+    displayLocalWeather();
+    displayRegionalWeather();
+}
+
+setInterval(()=> {
+    pgInitialize();
+}, 300000);
+
+pgInitialize();
