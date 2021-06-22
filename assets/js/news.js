fb0b0b96724a56632eb94399a80715edae0a72f7
Kelebet Engida <engida398@gmail.com>
2021-06-22 09:26:47 -0700

    Updated the links connecting to the api

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,23 +1,21 @@
-$(document).ready(function(){
+function displayNews() {
+  var queryURL = `https://gnews.io/api/v4/search?q=example&token=abacafece583fa895704071ecb610081`
+  
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.articles[0])
+      for (var i = 0; i < 9; i++) {
+        $("#displayheadline").append(`
+            <a href="${data.articles[i].url} class="news-headlines">${data.articles[i].title}</a>
+          `)
+        
+      }
 
+    })
 
-    var news;
+}
 
-    function searchnews(news){
-        var queryURL=   `https://gnews.io/api/v4/search?q=example&token=abacafece583fa895704071ecb610081`
-        fetch(queryURL)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data){
-          console.log(data);
-          
-        })
-    
-    }
-
-searchnews("the wall street journal")
-
-
-
-})
+displayNews();
