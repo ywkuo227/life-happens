c31727ff83c4f6f24a1c3ade18494c03faa46901
Megan Ekse <meganekse@gmail.com>
2021-06-21 20:51:29 -0700

    added name displyed on hub page, edited css styling on form labels and login page

--- assets/js/DateTime.js
+++ assets/js/DateTime.js
@@ -1,7 +1,8 @@
-timeEl = document.querySelector(".time");
-dateEl = document.querySelector(".date");
+var timeEl = document.querySelector(".time");
+var dateEl = document.querySelector(".date");
+var nameEl = $(".displayedName");
 
-
+nameEl.text(JSON.parse(localStorage.getItem("UsrInfo")).userName.firstName);
 function setDateTime() {
     var today = moment().format("[Today is] dddd, MMMM D, YYYY");
     
