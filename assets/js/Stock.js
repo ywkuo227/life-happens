0e6c93caa48c90f1748d83a7c9a287d5298e2750
Jeff Lynch <jlynch3@uw.edu>
2021-06-22 17:40:02 -0700

    first commit

--- assets/js/Stock.js
+++ assets/js/Stock.js
@@ -1,24 +1,33 @@
-$(document).ready(function(){
-
-
-});
-
-$(".viewBtn").click(function(){
-    $("#money").val()
-    console.log(data)
-})
-
-
-
-fetch("http://api.marketstack.com/v1/eod?access_key=bbb691e5ae8c59cd63c176ecacb21f80&symbols=AAPL")
-    .then(response => response.json())
-    .then(data => console.log(data));
- 
+var stockNews = document.getElementById("stock-news")
+var stockTicker = document.getElementById("stockTicker")
 
 
 
 
 
+// //stockNews.out.printin(data.data)
+
+// var stock = [0, 1, 2]
+// for (var i = 0; i < stock.length; i++)
+
+//     stockNews.out.printin(i + "" + stock[i]);
+
+function stockMarket(){
+    fetch("http://api.marketstack.com/v1/eod/latest?access_key=bbb691e5ae8c59cd63c176ecacb21f80&symbols=AAPL,MSFT,TSLA,AMZN")
+    .then(response => {
+        console.log(response)
+        return response.json()
+
+    })
+    .then(data => {console.log(data.data)
+        for(var i = 0; i < data.data.length; i++){
+            var listItem = document.createElement("li");
+            listItem.textContent= data.data[i].symbol +  " High: " + data.data[i].high + " Low: " + data.data[i].low;
+
+            stockTicker.append(listItem)
+        } 
+    });
+}
 
 
 
@@ -29,4 +38,8 @@ fetch("http://api.marketstack.com/v1/eod?access_key=bbb691e5ae8c59cd63c176ecacb2
 
 
 
+
+
+
+stockMarket()
 
